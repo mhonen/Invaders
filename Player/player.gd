96003f5a411a4d1c0ca11486511bd21acc186934
Mike Honen <103990968+mhonen@users.noreply.github.com>
2023-09-24 10:55:53 +0900

    Update player movement
    Update player.gd script to update player movement and to keep player on the screen.  I also added a new animated sprite for thrusters and updated player script with a new move and _ready function.
    
    Version: 0.2

--- Player/player.gd
+++ Player/player.gd
@@ -2,8 +2,16 @@ extends CharacterBody2D
 
 var speed = 300
 
+# Needed to add the ready function so thruster animation will work.
+# Probably will use a vonready variable later to get rid of funtion
+func _ready() -> void:
+	$Thrusters/AnimationPlayer.play("thrust")
+	
 func _process(delta):
 	
+	move(delta)
+	
+func move(delta):
 	if Input.is_action_pressed("left"):
 		position.x -= speed * delta 
 	if Input.is_action_pressed("right"):
@@ -12,5 +20,9 @@ func _process(delta):
 		position.y -= speed * delta
 	if Input.is_action_pressed("down"):
 		position.y += speed * delta
-	
 	move_and_slide()
+	
+	# Keeps player on the screen using clamp.
+	var viewRect = get_viewport_rect()
+	position.x = clamp(position.x, 200, viewRect.size.x - 200)
+	position.y = clamp(position.y, 150, viewRect.size.y - 150)
